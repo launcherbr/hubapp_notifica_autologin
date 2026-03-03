2bfd0d3cfdbe5680a179070688221050b026f133
Leandro Palmeira <leandropalmeira@hotmail.com>
2026-03-02 21:50:24 -0300

    Add files via upload

--- modules/addons/hubapp_notifica/hubapp_notifica.php
+++ modules/addons/hubapp_notifica/hubapp_notifica.php
@@ -49,7 +49,6 @@ function hubapp_notifica_output($vars) {
     echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">';
     require_once __DIR__ . '/lib/HubAppClient.php';
     
-    // Templates atualizados com autologin_url
     $templates = [
         'InvoiceCreated' => [
             'name' => 'Fatura Gerada', 
