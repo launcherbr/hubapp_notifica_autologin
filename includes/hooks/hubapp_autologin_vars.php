5a3058372afde5cb98fac7606ec37ac3ae64403f
Leandro Palmeira <leandropalmeira@hotmail.com>
2026-03-20 09:59:15 -0300

    Update hubapp_autologin_vars.php

--- includes/hooks/hubapp_autologin_vars.php
+++ includes/hooks/hubapp_autologin_vars.php
@@ -13,18 +13,27 @@ add_hook('EmailPreSend', 1, function($vars) {
         ->pluck('value', 'setting');
         
     $secretKey = $config['autologin_key'] ?? '';
-    $userid = $vars['userid'] ?? ($vars['relid'] ?? 0);
     
-    // Só gera se tiver chave e usuário válido
-    if ($secretKey && $userid > 0) {
+    // CORREÇÃO: Busca a ID do Cliente de forma segura, sem usar o $relid (que traz a ID da fatura)
+    $clientId = 0;
+    if (!empty($vars['client_id'])) {
+        $clientId = $vars['client_id'];
+    } elseif (!empty($vars['clientsdetails']['userid'])) {
+        $clientId = $vars['clientsdetails']['userid'];
+    } elseif (!empty($vars['userid'])) {
+        $clientId = $vars['userid'];
+    }
+    
+    // Só gera se tiver chave e um ID de cliente válido
+    if ($secretKey && $clientId > 0) {
         
         $systemUrl = rtrim(Capsule::table('tblconfiguration')->where('setting', 'SystemURL')->value('value'), '/');
         $expHours = (int)($config['expiration_hours'] ?? 72);
         
-        // Cria o Token
+        // Cria o Token com a ID correta do Cliente
         $header = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode(json_encode(['typ' => 'JWT', 'alg' => 'HS256'])));
         $payload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode(json_encode([
-            'uid' => (int)$userid,
+            'uid' => (int)$clientId,
             'exp' => time() + ($expHours * 3600),
             'iss' => 'HubApp'
         ])));
